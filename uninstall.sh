dfb38852e632baa209d236042e7c7d0b1b8c7efc
醜人多作怪 <77535932+nobody116@users.noreply.github.com>
2022-05-13 01:20:54 +0000

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,7 +0,0 @@
-docker stop ss-rust && docker stop watchtower
-docker rm -f ss-rust && docker rm -f watchtower
-systemctl stop docker && systemctl disable docker
-apt purge docker-ce docker-ce-cli containerd.io
-rm -rf /var/lib/docker
-rm -rf /var/lib/containerd
-rm -rf /etc/shadowsocks
